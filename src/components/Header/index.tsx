41f923a5de973a1de1231035eae92bdfaca6d36c
FlyBenja <aldanabenjamin185@gmail.com>
2024-11-11 19:24:15 -0600

    Cambiando imagen de logo de plantilla por el de UMG

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -56,7 +56,7 @@ const Header = (props: {
           {/* <!-- Hamburger Toggle BTN --> */}
 
           <Link className="block flex-shrink-0 lg:hidden" to="/admin/graficas">
-            <img src={LogoIcon} alt="Logo" />
+            <img src={LogoIcon} alt="Logo" width={55} />
           </Link>
         </div>
 
